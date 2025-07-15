bf76272d8160bd2933e0b8663131c536b7ce2f98
inightfulbriyan <32695570+insightfulbriyan@users.noreply.github.com>
2025-07-16 01:05:06 +0200

    Synced the color with existing FGC flag

---
+++ src/pages/info.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Rickroll = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center bg-black">
+      <iframe
+        width="1000"
+        height="562"
+        src="https://www.youtube.com/embed/3BFTio5296w?autoplay=1&mute=1&controls=0&modestbranding=1&rel=0&showinfo=0"
+        title="Rickroll"
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default Rickroll;
