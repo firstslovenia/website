343386e8714399f639da0d26314a47aa4d5b5d30
davidzindovic <david.zindovic@gmail.com>
2025-10-30 15:39:23 -0500

    Deploy website - based on f98968a422b11543c13e0502eef23012174a20a8

--- sl/assets/js/67c53663.94b6aa61.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkftcwebsite=self.webpackChunkftcwebsite||[]).push([[5264],{70281:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/sl/fgc/blog/page/5","page":5,"postsPerPage":10,"totalPages":5,"totalCount":42,"previousPage":"/sl/fgc/blog/page/4","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
